3f43293bf6c8e92bcd307a4461619a9f4508a13e
Sophie <wenxi1988@hotmail.com>
2022-01-12 20:25:45 -0500

    basic structure

--- githubsearch/.history/src/App_20220112201651.js
+++ githubsearch/.history/src/App_20220112201651.js
@@ -15,31 +15,31 @@ export default class App extends Component {
       <div className="row">
         <div className="card">
           <a href="https://github.com/reactjs" target="_blank">
-            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width:'100px'}}/>
+            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style='width: 100px'/>
           </a>
           <p className="card-text">reactjs</p>
         </div>
         <div className="card">
           <a href="https://github.com/reactjs" target="_blank">
-            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width:'100px'}}/>
+            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style='width: 100px'/>
           </a>
           <p className="card-text">reactjs</p>
         </div>
         <div className="card">
           <a href="https://github.com/reactjs" target="_blank">
-            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width:'100px'}}/>
+            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style='width: 100px'/>
           </a>
           <p className="card-text">reactjs</p>
         </div>
         <div className="card">
           <a href="https://github.com/reactjs" target="_blank">
-            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width:'100px'}}/>
+            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style='width: 100px'/>
           </a>
           <p className="card-text">reactjs</p>
         </div>
         <div className="card">
           <a href="https://github.com/reactjs" target="_blank">
-            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width:'100px'}}/>
+            <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style='width: 100px'/>
           </a>
           <p className="card-text">reactjs</p>
         </div>
