6d5cb6b2b3cd80fb1fc93a891fe3ef54d54db5d0
Sophie <wenxi1988@hotmail.com>
2022-01-12 21:00:01 -0500

    static html

---
+++ githubsearch/.history/src/Components/List/index_20220112205824.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react'
+import './index.css'
+export default class List extends Component {
+    render() {
+        return (
+            <div className="row">
+            <div className="card">
+              <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
+                <img alt="headPhoto" src="../../../public/images/649px-TUX_G2.svg.jpg" style={{width:'100px'}}/>
+              </a>
+              <p className="card-text">reactjs</p>
+            </div>
+            <div className="card">
+              <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
+                <img alt="headPhoto" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width:'100px'}}/>
+              </a>
+              <p className="card-text">reactjs</p>
+            </div>
+            <div className="card">
+              <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
+                <img alt="headPhoto" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width:'100px'}}/>
+              </a>
+              <p className="card-text">reactjs</p>
+            </div>
+            <div className="card">
+              <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
+                <img alt="headPhoto" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width:'100px'}}/>
+              </a>
+              <p className="card-text">reactjs</p>
+            </div>
+            <div className="card">
+              <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
+                <img alt="headPhoto" src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width:'100px'}}/>
+              </a>
+              <p className="card-text">reactjs</p>
+            </div>
+            </div>
+        )
+    }
+}
