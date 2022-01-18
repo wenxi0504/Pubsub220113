6883394418f34a1c9a920ed4d975f5b5f899230b
Sophie <wenxi1988@hotmail.com>
2022-01-18 00:34:58 -0500

    housekeeping

---
+++ githubsearch/11_src_sendsearchandstatetoroute/pages/Home/Message/index.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import {Link,Route,Routes} from "react-router-dom";
+import Details from "./Details";
+
+export default class Message extends Component {
+  state = {
+    messageArr: [
+      { id: "01", title: "msg1" },
+      { id: "02", title: "msg2" },
+      { id: "03", title: "msg3" },
+    ]
+  }
+
+  render() {
+    const{messageArr}=this.state
+    return (
+      <div>
+        <ul>
+          {messageArr.map((msgObj) => {
+            return (
+              <li key={msgObj.id}>
+                {/* pass params to route component */}
+                <Link to={`/home/message/details/${msgObj.id}/${msgObj.title}`}>{msgObj.title}</Link>
+              </li>
+            );
+          })}
+        </ul>
+        <hr/>
+        {/* register route */}
+        {/* receive param */}
+        <Routes>
+        <Route path="details/:id/:title" element={<Details/>}></Route>
+        </Routes>
+        
+       
+      </div>
+    );
+  }
+}
